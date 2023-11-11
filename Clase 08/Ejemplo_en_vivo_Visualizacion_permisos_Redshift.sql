60da5549fd9de386e2db6f836e7a7f1263c67e61
Ariel Chumbita <ariel.chumbita@gmail.com>
2023-11-11 00:49:35 -0300

    .

--- Clase 08/Ejemplo_en_vivo_Visualizacion_permisos_Redshift.sql
+++ Clase 08/Ejemplo_en_vivo_Visualizacion_permisos_Redshift.sql
@@ -1,3 +1,5 @@
+--FORMA CORTA PARA VISUALIZAR PERMISOS EN REDSHIFT
+
 SELECT
     u.usename,
     s.schemaname,
@@ -8,5 +10,25 @@ FROM
 CROSS JOIN
     (SELECT DISTINCT schemaname FROM pg_tables) s
 WHERE
-    u.usename = 'dafbustosus_coderhouse'
-    AND s.schemaname = 'dafbustosus_coderhouse'
+    u.usename = 'ariel_chumbita_coderhouse'
+    AND s.schemaname = 'ariel_chumbita_coderhouse'
+    
+ --Salida: si da V es que tiennes acceso.
+    
+    
+    --FORMA COMPLEJA DE VISUALIZAR PERMISOS EN REDSHIFT
+SELECT
+    u.usename,
+    t.schemaname||'.'||t.tablename,
+    has_table_privilege(u.usename,t.tablename,'select') AS user_has_select_permission,
+    has_table_privilege(u.usename,t.tablename,'insert') AS user_has_insert_permission,
+    has_table_privilege(u.usename,t.tablename,'update') AS user_has_update_permission,
+    has_table_privilege(u.usename,t.tablename,'delete') AS user_has_delete_permission,
+    has_table_privilege(u.usename,t.tablename,'references') AS user_has_references_permission
+FROM
+    pg_user u
+CROSS JOIN
+    pg_tables t
+WHERE
+    u.usename = 'ariel_chumbita_coderhouse'
+    --AND s.schemaname = 'ariel_chumbita_coderhouse'
