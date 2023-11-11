533b548a7d11d697e52014d45af23eccc8257a7c
Ariel Chumbita <ariel.chumbita@gmail.com>
2023-11-11 01:42:35 -0300

    .

--- Clase 08/Seguridad_basica_Redshift.sql
+++ Clase 08/Seguridad_basica_Redshift.sql
@@ -19,7 +19,7 @@ SELECT * FROM my_secure_schema.my_secure_table;
 
 
 --- Verificar si tiene acceso al esquema
-SELECT schema_name FROM information_schema.schemata 
+SELECT schema_name FROM information_schema.schemata  --Consulto permisos para un usuario
 WHERE schema_owner = 'data_scientist';
 
 -- Verificar todos mis esquemas
@@ -33,3 +33,22 @@ SELECT usename FROM pg_user
 SELECT grantee, privilege_type 
 FROM information_schema.table_privileges 
 WHERE grantee = 'data_scientist';
+
+
+-----------SEGURIDAD COLUMNAS-----------
+
+-- Momento 1: Otorgar acceso al usuario para todas las columnas, excepto el Número de Seguro Social (ssn) para el usuario
+GRANT ALL ON SCHEMA my_secure_schema TO data_scientist; --Acceso al esquema
+GRANT SELECT(name, dob, zip) ON my_secure_schema.my_secure_table TO data_scientist; --Acceso a parte de la tabla
+
+REVOKE ALL ON SCHEMA my_secure_schema TO data_scientist; --Quito acceso al esquema
+
+-- Momento 2: Conectarse al clúster con usuario data_scientist. 
+-- Ejecute una selección en la tabla con y sin el Número de Seguro Social. Observen la diferencia
+SELECT name, dob, zip
+FROM my_secure_schema.my_secure_table;
+
+SELECT name, dob, zip, ssn
+FROM my_secure_schema.my_secure_table;
+
+--La primera consulta debera arrojar 0 filas mientras que la segunda mostrara un error
