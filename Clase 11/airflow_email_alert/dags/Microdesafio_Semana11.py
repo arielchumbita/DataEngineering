3b7f431e69eed317277cea055d83c8aaacfcce6a
Ariel Chumbita <ariel.chumbita@gmail.com>
2023-12-28 23:49:32 -0300

    .

--- Clase 11/airflow_email_alert/dags/Microdesafio_Semana11.py
+++ Clase 11/airflow_email_alert/dags/Microdesafio_Semana11.py
@@ -3,11 +3,11 @@ from email import message
 from airflow.models import DAG, Variable
 from airflow.operators.python_operator import PythonOperator
 
-import smtplib
+import smtplib #Libreria que da el proceso para enviar el mensaje
 
-pais=['Argentina','Brasil','Colombia','Chile','Paraguay','Uruguay','Venezuela','Peru','Ecuador','Bolivia']
-acronimo= ['AR','BR','CO','CL','PY','UR','VE','PE','EC','BO']
-lista_fin_mundo=[2040,2080,2095,2100,2089,2093,2054,2078,2079,2083]
+pais=['Argentina','Brasil','Colombia','Chile','Paraguay','Uruguay','Venezuela','Peru','Ecuador','Bolivia','Mexico']
+acronimo= ['AR','BR','CO','CL','PY','UR','VE','PE','EC','BO','MX']
+lista_fin_mundo=[2040,2080,2095,2100,2089,2093,2054,2078,2079,2083,2071]
 
 texto=[]
 
@@ -22,19 +22,19 @@ def enviar():
     try:
         x=smtplib.SMTP('smtp.gmail.com',587)
         x.starttls()
-        x.login('davidbustosusta@gmail.com','xxxxxxxxxx')
+        x.login('ariel.chumbita@gmail.com','wprozivegdgkpnnn') # Cambia tu contraseña !!!!!!!!
         subject='Fechas fin del mundo'
         body_text=final
         message='Subject: {}\n\n{}'.format(subject,body_text)
-        x.sendmail('davidbustosusta@gmail.com','aileddelacruzpaez@gmail.com',message)
+        x.sendmail('ariel.chumbita@gmail.com','ariel-2401@hotmail.com',message) #Mail remitente y receptor
         print('Exito')
     except Exception as exception:
         print(exception)
         print('Failure')
 
 default_args={
-    'owner': 'DavidBU',
-    'start_date': datetime(2023,5,13)
+    'owner': 'ArielChumbita',
+    'start_date': datetime(2023,12,28)
 }
 
 with DAG(
